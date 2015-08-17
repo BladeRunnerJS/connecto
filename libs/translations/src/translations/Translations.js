1f3112d975484a19f22719758be0e89f425273d6
danielo <daniel.owen@caplin.com>
2015-08-17 12:06:49 +0100

    adding missing french translations

--- libs/translations/src/translations/Translations.js
+++ libs/translations/src/translations/Translations.js
@@ -4,218 +4,356 @@ var Translations = function() {
 };
 
 Translations.prototype.animals = [
-			{english: "dog",
+			{
+			english: "dog",
 			french: "le chien",
-			spanish: "el perro"},
-			{english: "bear",
+			spanish: "el perro"
+			},
+			{
+			english: "bear",
 			french: "l'ours",
-			spanish: "el oso"},
-			{english: "bird" ,
+			spanish: "el oso"
+			},
+			{
+			english: "bird" ,
 			french: "l'oiseau",
-			spanish: "el pajaro"},
-			{english: "cat",
+			spanish: "el pajaro"
+			},
+			{
+			english: "cat",
 			french: "le chat",
-			spanish: "el gato"},
-			{english: "chicken",
+			spanish: "el gato"
+			},
+			{
+			english: "chicken",
 			french: "le poulet",
-			spanish: "la gallina"},
-			{english: "cow",
+			spanish: "la gallina"
+			},
+			{
+			english: "cow",
 			french: "la vache",
-			spanish: "la vaca"},
-			{english: "dolphin",
+			spanish: "la vaca"
+			},
+			{
+			english: "dolphin",
 			french: "le dauphin",
-			spanish: "el delfin"},
-			{english: "donkey",
+			spanish: "el delfin"
+			},
+			{
+			english: "donkey",
 			french: "l'âne",
-			spanish: "el burro"},
-			{english: "duck",
+			spanish: "el burro"
+			},
+			{
+			english: "duck",
 			french: "le canard",
-			spanish: "el pato"},
-			{english: "elephant",
+			spanish: "el pato"
+			},
+			{
+			english: "elephant",
 			french: "l'éléphant",
-			spanish: "el elefante"},
-			{english: "fish",
+			spanish: "el elefante"
+			},
+			{
+			english: "fish",
 			french: "le poisson",
-			spanish: "el pez"},
-			{english: "fox",
+			spanish: "el pez"
+			},
+			{
+			english: "fox",
 			french: "le renard",
-			spanish: "el zorro"},
-			{english: "frog",
+			spanish: "el zorro"
+			},
+			{
+			english: "frog",
 			french: "la grenouille",
-			spanish: "la rana"},
-			{english: "giraffe",
+			spanish: "la rana"
+			},
+			{
+			english: "giraffe",
 			french: "la girafe",
-			spanish: "la jirafa"},
-			{english: "guineapig",
+			spanish: "la jirafa"
+			},
+			{
+			english: "guineapig",
 			french: "le cobaye",
-			spanish: "la cobaya"},
-			{english: "hamster",
+			spanish: "la cobaya"
+			},
+			{
+			english: "hamster",
 			french: "hamster",
-			spanish: "hamster"},
-			{english: "horse",
+			spanish: "hamster"
+			},
+			{
+			english: "horse",
 			french: "le cheval",
-			spanish: "el caballo"},
-			{english: "kangaroo",
+			spanish: "el caballo"
+			},
+			{
+			english: "kangaroo",
 			french: "le kangourou",
-			spanish: "el canguro"},
-			{english: "lion",
+			spanish: "el canguro"
+			},
+			{
+			english: "lion",
 			french: "le lion",
-			spanish: "el leon"},
-			{english: "monkey",
+			spanish: "el leon"
+			},
+			{
+			english: "monkey",
 			french: "le singe",
-			spanish: "el mono"},
-			{english: "mouse",
+			spanish: "el mono"
+			},
+			{
+			english: "mouse",
 			french: "la souris",
-			spanish: "el raton"},
-			{english: "killerwhale",
+			spanish: "el raton"
+			},
+			{
+			english: "killerwhale",
 			french: "l'killerwhale",
-			spanish: "la orca"},
-			{english: "penguin",
+			spanish: "la orca"
+			},
+			{
+			english: "penguin",
 			french: "le pingouin",
-			spanish: "el pingüino"},
-			{english: "pig",
+			spanish: "el pingüino"
+			},
+			{
+			english: "pig",
 			french: "le cochon",
-			spanish: "el cerdo"},
-			{english: "rabbit",
+			spanish: "el cerdo"
+			},
+			{
+			english: "rabbit",
 			french: "le lapin",
-			spanish: "el conejo"},
-			{english: "shark",
+			spanish: "el conejo"
+			},
+			{
+			english: "shark",
 			french: "le requin",
-			spanish: "el tiburón"},
-			{english: "sheep",
+			spanish: "el tiburón"
+			},
+			{
+			english: "sheep",
 			french: "les moutons",
-			spanish: "la oveja"},
-			{english: "snake",
+			spanish: "la oveja"
+			},
+			{
+			english: "snake",
 			french: "le serpent",
-			spanish: "la serpiente"},
-			{english: "squirrel",
+			spanish: "la serpiente"
+			},
+			{
+			english: "squirrel",
 			french: "l'écureuil",
-			spanish: "la ardilla"},
-			{english: "tiger",
+			spanish: "la ardilla"
+			},
+			{
+			english: "tiger",
 			french: "le tigre",
-			spanish: "el tigre"},
-			{english: "turtle",
+			spanish: "el tigre"
+			},
+			{
+			english: "turtle",
 			french: "la tortue",
-			spanish: "la tortuga"},
-			{english: "whale",
+			spanish: "la tortuga"
+			},
+			{
+			english: "whale",
 			french: "la baleine",
-			spanish: "la ballena"},
-			{english: "wildboar",
+			spanish: "la ballena"
+			},
+			{
+			english: "wildboar",
 			french: "le sanglier",
-			spanish: "el jabalí"},
-			{english: "wolf",
+			spanish: "el jabalí"
+			},
+			{
+			english: "wolf",
 			french: "le loup",
-			spanish: "el zorro"},
-			{english: "zebra",
+			spanish: "el zorro"
+			},
+			{
+			english: "zebra",
 			french: "zèbre",
 			spanish: "la cebra"
 			}
 ];
 	
 Translations.prototype.sports = [
-			{english: "archery",
+			{
+			english: "archery",
 			french: "l'arc",
-			spanish: "el tiro con arco"},
-			{english: "badminton",
+			spanish: "el tiro con arco"
+			},
+			{
+			english: "badminton",
 			french: "badminton",
-			spanish: "el bádminton"},
-			{english: "baseball" ,
+			spanish: "el bádminton"
+			},
+			{
+			english: "baseball" ,
 			french: "le baseball",
-			spanish: "el béisbol"},
-			{english: "basketball",
+			spanish: "el béisbol"
+			},
+			{
+			english: "basketball",
 			french: "le basket-ball",
-			spanish: "el baloncesto"},
-			{english: "bowling",
+			spanish: "el baloncesto"
+			},
+			{
+			english: "bowling",
 			french: "bowling",
-			spanish: "los bolos"},
-			{english: "boxing",
+			spanish: "los bolos"
+			},
+			{
+			english: "boxing",
 			french: "la boxe",
-			spanish: "el boxeo"},
-			{english: "canoeing",
+			spanish: "el boxeo"
+			},
+			{
+			english: "canoeing",
 			french: "le canoë",
-			spanish: "el piragüismo"},
-			{english: "cycling",
+			spanish: "el piragüismo"
+			},
+			{
+			english: "cycling",
 			french: "le vélo",
-			spanish: "el ciclismo"},
-			{english: "dancing",
+			spanish: "el ciclismo"
+			},
+			{
+			english: "dancing",
 			french: "la danse",
-			spanish: "el baile"},
-			{english: "fencing",
+			spanish: "el baile"
+			},
+			{
+			english: "fencing",
 			french: "la clôture",
-			spanish: "la esgrima"},
-			{english: "fishing",
+			spanish: "la esgrima"
+			},
+			{
+			english: "fishing",
 			french: "la pêche",
-			spanish: "La Pesca"},
-			{english: "football",
+			spanish: "La Pesca"
+			},
+			{
+			english: "football",
 			french: "le football",
-			spanish: "el fútbol"},
-			{english: "golf",
+			spanish: "el fútbol"
+			},
+			{
+			english: "golf",
 			french: "le terrain",
-			spanish: "golf el"},
-			{english: "gymnastics",
+			spanish: "golf el"
+			},
+			{
+			english: "gymnastics",
 			french: "la gymnastique",
-			spanish: "La gimnasia"},
-			{english: "handball",
+			spanish: "La gimnasia"
+			},
+			{
+			english: "handball",
 			french: "le handball",
-			spanish: "el balonmano"},
-			{english: "hiking",
+			spanish: "el balonmano"
+			},
+			{
+			english: "hiking",
 			french: "la randonnée",
-			spanish: "el senderismo"},
-			{english: "hockey",
+			spanish: "el senderismo"
+			},
+			{
+			english: "hockey",
 			french: "le hockey",
-			spanish: "el hockey"},
-			{english: "horseriding",
+			spanish: "el hockey"
+			},
+			{
+			english: "horseriding",
 			french: "le cheval",
-			spanish: "la equitación"},
-			{english: "iceskating",
+			spanish: "la equitación"
+			},
+			{
+			english: "iceskating",
 			french: "la patinoire",
-			spanish: "el patinaje sobre hielo"},
-			{english: "karate",
+			spanish: "el patinaje sobre hielo"
+			},
+			{
+			english: "karate",
 			french: "karaté",
-			spanish: "el karaté"},
-			{english: "parachuting",
+			spanish: "el karaté"
+			},
+			{
+			english: "parachuting",
 			french: "parachutage",
-			spanish: "el Paracaidismo"},
-			{english: "rollerskating",
+			spanish: "el Paracaidismo"
+			},
+			{
+			english: "rollerskating",
 			french: "le roller",
-			spanish: "el patinaje sobre ruedas"},
-			{english: "rowing",
+			spanish: "el patinaje sobre ruedas"
+			},
+			{
+			english: "rowing",
 			french: "l'aviron",
-			spanish: "el remo"},
-			{english: "rugby",
+			spanish: "el remo"
+			},
+			{
+			english: "rugby",
 			french: "le rugby",
-			spanish: "el rugby"},
-			{english: "running",
+			spanish: "el rugby"
+			},
+			{
+			english: "running",
 			french: "la gestion",
-			spanish: "el pied"},
-			{english: "sailing",
+			spanish: "el pied"
+			},
+			{
+			english: "sailing",
 			french: "le voile",
-			spanish: "la navegación"},
-			{english: "scubadiving",
+			spanish: "la navegación"
+			},
+			{
+			english: "scubadiving",
 			french: "la plongée sous-marine",
-			spanish: "el buceo"},
-			{english: "skiing",
+			spanish: "el buceo"
+			},
+			{
+			english: "skiing",
 			french: "le ski",
-			spanish: "el esquí"},
-			{english: "snowboarding",
+			spanish: "el esquí"
+			},
+			{
+			english: "snowboarding",
 			french: "la planche à neige",
-			spanish: "el de snowboard"},
-			{english: "surfing",
+			spanish: "el de snowboard"
+			},
+			{
+			english: "surfing",
 			french: "surf",
-			spanish: "el surfer"},
-			{english: "swimming",
+			spanish: "el surfer"
+			},
+			{
+			english: "swimming",
 			french: "la piscine",
-			spanish: "la natación"},
-			{english: "tabletennis",
+			spanish: "la natación"
+			},
+			{
+			english: "tabletennis",
 			french: "le ping-pong",
-			spanish: "el ping-pong"},
-			{english: "tennis",
+			spanish: "el ping-pong"
+			},
+			{
+			english: "tennis",
 			french: "tennis",
-			spanish: "tenis el"},
-			{english: "volleyball",
+			spanish: "tenis el"
+			},
+			{
+			english: "volleyball",
 			french: "volley-ball",
-			spanish: "el voleibol"},
-			{english: "wrestling",
+			spanish: "el voleibol"
+			},
+			{
+			english: "wrestling",
 			french: "la lutte",
 			spanish: "La Lucha"
 			}
@@ -224,143 +362,178 @@ Translations.prototype.sports = [
 Translations.prototype.transport = [
 			{
 				english: "airplane",
-				spanish:"el avión"
+				french: "l'avion",
+				spanish:"el avión"			
 			},
 			{
 				english:"ambulance",
+				french: "l'ambulance",				
 				spanish: "la ambulancia"
 			},
 			{
 				english:"bicycle" ,
+				french: "la bicyclette",
 				spanish: "la bicicleta"
 			},
 			{
 				english:"boat",
+				french: "le bateau",
 				spanish: "la barca"
 			},
 			
 			{
 				english:"bus",
+				french: "le bus",
 				spanish: "el autobús"
 			},
 			{
 				english:"canoe",
+				french: "la pirogue",
 				spanish: "la canoa"
 			},
 			{
 				english:"car",
+				french: "",
 				spanish: "el coche"
 			},
 			{
 				english:"caravan",
+				french: "",
 				spanish: "la caravana"
 			},
 			{
 				english:"coach",
+				french: "",
 				spanish: "el autocar"
 			},
 			{
 				english:"cruise",
+				french: "",
 				spanish: "el crucero"
 			},
 			{
 				english:"firetruck",
+				french: "la voiture",
 				spanish: "el camión de bomberos"
 			},
 			{
 				english:"helicopter",
+				french: "",
 				spanish: "el helicóptero"
 			},
 			{
 				english:"horsecart",
+				french: "l'hélicoptère",
 				spanish: "el carruaje"
 			},
 			{
-				english:"hotairballoon",
+				english:"hot air balloon",
+				french: "le ballon à air chaud",
 				spanish: "el globo de aire caliente"
 			},
 			{
 				english:"motorcycle",
+				french: "la moto",
 				spanish: "la motocicleta"
 			},
 			{
-				english:"motorscooter",
+				english:"motor scooter",
+				french: "le scooter",
 				spanish: "la moto"
 			},
 			{
 				english:"rocket",
+				french: "",
 				spanish: "el cohete"
 			},
 			{
 				english:"rollerblades",
+				french: "",
 				spanish: "los patines en línea"
 			},
 			{
 				english:"rollerskates",
+				french: "",
 				spanish: "los patines sobre ruedas"
 			},
 			{
 				english:"sailingboat",
+				french: "la fusée",
 				spanish: "el velero"
 			},
 			{
 				english:"ship",
+				french: "le bateau",
 				spanish: "el barco"
 			},
 			{
 				english:"skateboard",
+				french: "",
 				spanish: "el monopatín"
 			},
 			{
 				english:"spaceship",
+				french: "",
 				spanish: "la nave espacial"
 			},
 			{
 				english:"speedboat",
+				french: "la planche à roulettes",
 				spanish: "la lancha"
 			},
 			{
 				english:"submarine",
+				french: "le sous-marin",
 				spanish: "el submarino"
 			},
 			{
 				english:"taxi",
+				french: "",
 				spanish: "el taxi"
 			},
 			{
 				english:"tractor",
+				french: "",
 				spanish: "el tractor"
 			},
 			{
 				english:"train",
+				french: "le taxi",
 				spanish: "el tren"
 			},
 			{
 				english:"tram",
+				french: "le tramway",
 				spanish: "el tranvía"
 			},
 			{
 				english:"tricycle",
+				french: "le tricycle",
 				spanish: "el triciclo"
 			},
 			{
 				english:"truck",
+				french: "le camion",
 				spanish: "el camión"
 			},
 			{
 				english:"tube",
+				french: "le métro",
 				spanish: "el metro"
 			},
 			{
 				english:"van",
+				french: "le van",
 				spanish: "la camioneta"
 			},
 			{
 				english:"wheelchair",
+				french: "le fauteuil roulant",
 				spanish: "la silla de ruedas"
 			},
 			{
 				english:"yacht",
+				french: "le yacht",
 				spanish: "el yate"
 			}
 ];
@@ -368,143 +541,178 @@ Translations.prototype.transport = [
 Translations.prototype.vegetables = [
 			{
 				english: "artichoke",
+				french: "l'artichaut",
 				spanish:"la alcachofa"
 			},
 			{
 				english:"asparagus",
+				french: "les asperges",
 				spanish: "el espárrago"
 			},
 			{
 				english:"beans" ,
+				french: "les haricots",
 				spanish: "la alubia"
 			},
 			{
 				english:"beetroot",
+				french: "la betterave",
 				spanish: "la remolacha"
 			},
 			
 			{
-				english:"bokchoy",
+				english:"bok choy",
+				french: "le chou chinois",
 				spanish: "el repollo chino"
 			},
 			{
 				english:"broccoli",
+				french: "les brocolis",
 				spanish: "el brócoli"
 			},
 			{
-				english:"brusselssprouts",
+				english:"brussels sprouts",
+				french: "les choux de Bruxelles",
 				spanish: "las coles de bruselas"
 			},
 			{
 				english:"cabbage",
+				french: "le chou",
 				spanish: "la col"
 			},
 			{
 				english:"carrot",
+				french: "la carotte",
 				spanish: "la zanahoria"
 			},
 			{
 				english:"celery",
+				french: "le céleri",
 				spanish: "el apio"
 			},
 			{
 				english:"chickpeas",
+				french: "les pois chiches",
 				spanish: "los garbanzos"
 			},
 			{
 				english:"corn",
+				french: "le maïs",
 				spanish: "el maíz"
 			},
 			{
 				english:"garlic",
+				french: "l'ail",
 				spanish: "el ajo"
 			},
 			{
 				english:"ginger",
+				french: "le gingembre",
 				spanish: "el jengibre"
 			},
 			{
 				english:"greenbeans",
+				french: "les haricots verts",
 				spanish: "las judías verdes"
 			},
 			{
 				english:"radish",
+				french: "le radis",
 				spanish: "el rábano"
 			},
 			{	
 				english:"horseradish",
+				french: "le raifort",
 				spanish: "el rábano picante"
 			},
 			{
 				english:"leek",
+				french: "le poireau",
 				spanish: "el puerro"
 			},
 			{
 				english:"lentils",
+				french: "les lentilles",
 				spanish: "las lentejas"
 			},
 			{
 				english:"lettuce",
+				french: "la laitue",
 				spanish: "la lechuga"
 			},
 			{
 				english:"mushroom",
+				french: "le champignon",
 				spanish: "el champiñón"
 			},
 			{
 				english:"onion",
+				french: "un oignon",
 				spanish: "la cebolla"
 			},
 			{
-				english:"springonion",
+				english:"spring onion",
+				french: "l'oignon de printemps",
 				spanish: "el cebollín"
 			},
 			{
 				english:"parsley",
+				french: "le persil",
 				spanish: "el perejil"
 			},
 			{
 				english:"parsnip",
+				french: "panais",
 				spanish: "la charavía"
 			},
 			{
 				english:"peas",
+				french: "les petits pois",
 				spanish: "los guisantes"
 			},
 			{
 				english:"pepper",
+				french: "le poivre",
 				spanish: "el pimiento"
 			},
 			{
 				english:"potato",
+				french: "la pomme de terre",
 				spanish: "la patata"
 			},
 			{
 				english:"pumpkin",
+				french: "la citrouille",
 				spanish: "la calabaza"
 			},
 			{
 				english:"shallot",
+				french: "l'échalote",
 				spanish: "el chalote"
 			},
 			{
 				english:"spinach",
+				french: "les épinards",
 				spanish: "la espinaca"
 			},
 			{
 				english:"tomato",
+				french: "la tomate",
 				spanish: "el tomate"
 			},
 			{
 				english:"turnip",
+				french: "le navet",
 				spanish: "el nabo"
 			},
 			{
 				english:"swede",
+				french: "le rutabaga",
 				spanish: "el nabo sueco"
 			},
 			{
 				english:"watercress",
+				french: "le cresson",
 				spanish: "el berro"
 			}
 ];
